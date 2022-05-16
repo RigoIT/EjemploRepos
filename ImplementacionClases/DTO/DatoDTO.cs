0167b9d842ca1d8d37528c9a5f82c9d14547dcc9
RigoIT <rodrigo.gutierrez48@inacapmail.cl>
2022-05-15 23:22:01 -0400

    Primer Commit

---
+++ ImplementacionClases/DTO/DatoDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementacionClases.DTO
+{
+    public class DatoDTO
+    {
+        private int id;
+        private float flujo;
+        private float nivel;
+        private float temperatura;
+        private float voltaje;
+        private BancoDTO banco;
+        private CeldaDTO celda;
+
+        public int Id { get => id; set => id = value; }
+        public float Flujo { get => flujo; set => flujo = value; }
+        public float Nivel { get => nivel; set => nivel = value; }
+        public float Temperatura { get => temperatura; set => temperatura = value; }
+        public float Voltaje { get => voltaje; set => voltaje = value; }
+        public float TemperaturaFarenheit { get => temperatura * (9 / 5) + 32; }
+        public BancoDTO Banco { get => banco; set => banco = value; }
+        public CeldaDTO Celda { get => celda; set => celda = value; }
+        // (0 °C × 9/5) + 32 = 32 °F
+    }
+}
---
+++ ImplementacionClases/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using ImplementacionClases.Utils;
+
+// Ejemplo de Clase estática:
+// Console.WriteLine(ConsoleUtils.Saludar("Enrique"));
+
+/* 
+ * Ejemplo de escritura por línea y por palabra(s)
+ * Console.Write("Hola");
+Console.Write(" Mundo"); */
+
+/* Ejemplo de cambios de colores en consola 
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.ForegroundColor = ConsoleColor.Green;
+ */
+
+/* Ejemplo Entrada / salida consola 
+Console.Write("Escribe un nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Ha escrito {nombre}"); */
+
+while (Menu())
+{
+    Console.ReadKey(); // pausa, solicitar la entrada de una tecla
+}
+
+static bool Menu()
+{
+    bool continuar = true;
+
+    Console.Clear(); // Limpia la pantalla
+    Console.Title = "Ejercicio Menú + Clases";
+
+    Console.WriteLine("Menú de opciones");
+    Console.WriteLine("================");
+    Console.WriteLine("1) Listar datos");
+    Console.WriteLine("2) Agregar datos");
+    Console.WriteLine("");
+    Console.WriteLine("0) Salir");
+
+    string opcion = Console.ReadLine().Trim(); // " 1 " => "1"
+
+    switch (opcion)
+    {
+        case "1":
+            Console.WriteLine("Escogió la opción 1");
+            break;
+        case "2":
+            Console.WriteLine("Escogió la opción 2");
+            OpcionInsertar();
+            break;
+        case "0":
+            Console.WriteLine("Saliendo del programa ...");
+            continuar = false;
+            break;
+        default:
+            Console.WriteLine("Opción no válida");
+            break;
+    }
+    
+    return continuar;
+}
+
+static void OpcionInsertar()
+{
+    /*
+     *  int id
+        float flujo
+        float nivel
+        float temperatura
+        float voltaje
+     */
+    Console.WriteLine("bla bla");
+    string respuesta = Console.ReadLine();
+
+    // EJEMPLOS DE CONVERSION
+    // 1) Cualquier cosa a string
+    int i = 5;
+    i.ToString();
+    0xfb00.ToString(); // hexadecimal a string
+
+    // Utilizando la clase Convert (sirve a varios tipos)
+    int convertido = Convert.ToInt32("-100");
+
+    // Conversión explícita
+    int test = 12345;
+    float conDecimales = (float) test;
+
+    // Conversión implícita
+    int otroTest = 123456;
+    float otroDecimal = otroTest; //123456.0
+   
+}
